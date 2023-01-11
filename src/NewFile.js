f9b3a4a4e9dd4df1abcefdfc4c3c2cb7cb50f615
Akashay Anand <akashayanand1@gmail.com>
2023-01-11 14:19:35 +0530

    First commit

---
+++ src/NewFile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./NewFile.css"
+
+function NewFile() {
+    return (<div className='newF'>
+        <h1 className='newF'> Hello Anand </h1>
+        </div>
+    );
+    
+    
+}
+
+export default NewFile;
